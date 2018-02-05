89c1ba9394eb7a1519ee79618703822f90ca235f
paridhi <paridhi815@gmail.com>
2018-02-05 13:36:56 +0530

    #TC5 Test for adding unsuccessfull http get request

--- api1.test.js
+++ api1.test.js
@@ -8,4 +8,11 @@ describe('Tests for checking the logging of data received from HTTP Get request'
     };
     accessHTTPAsync('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', callback);
   });
+  test('Verify data returned for failed HTTP request', (done) => {
+    const callback = (data) => {
+      expect(data).toMatch('ERROR');
+      done();
+    };
+    accessHTTPAsync('http://httpstat.us/404', callback);
+  });
 });
