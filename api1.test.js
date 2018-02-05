79d4ecea876f0c00938108adc9cf8c057efb7659
paridhi <paridhi815@gmail.com>
2018-02-05 13:36:24 +0530

    #TC4 added test case for successful http get request

---
+++ api1.test.js
@@ -0,0 +1,11 @@
+const accessHTTPAsync = require('./api1');
+
+describe('Tests for checking the logging of data received from HTTP Get request', () => {
+  test('Verify data returned for successful HTTP request', (done) => {
+    const callback = (data) => {
+      expect(data).toMatch('DATA');
+      done();
+    };
+    accessHTTPAsync('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', callback);
+  });
+});
