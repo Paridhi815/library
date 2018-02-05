772f6b85051b1a8e6cb5c0a0abef53156c393deb
paridhi <paridhi815@gmail.com>
2018-02-05 11:28:08 +0530

    #TC1 Server Started.Test case written for valid request path.

---
+++ serverBooks.js
@@ -0,0 +1,33 @@
+const inert = require('inert');
+// const path = require('path');
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(3000),
+});
+
+server.register(inert, (err) => {
+  if (err) { throw err; }
+});
+
+// add route
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: {
+    file: './allBooks.txt',
+  },
+});
+
+// to make the server listen to the assigned port
+if (!module.parent) {
+  server.start((err) => {
+    if (err) { throw err; }
+    console.log('Server running at:', server.info.uri);
+  });
+}
+module.exports = server;
+
---
+++ serverBooks.test.js
@@ -0,0 +1,17 @@
+const server = require('./serverBooks');
+const fs = require('fs');
+
+// const html = fs.readFileSync('./allBooks.txt', 'UTF8');
+
+describe('Testing Hapi Using Inject:', () => {
+  test('Check Response status for valid path:', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/',
+    };
+    server.inject(options, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+});
